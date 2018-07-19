190f01d5fa5a0eed929c0778648c63a917c2086c
FatherOfLove <jerryhuang6666@gmail.com>
2018-07-19 15:07:59 -0700

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,17 @@
   'use strict';
   var FORM_SELECTOR = '[data-coffee-order="form"]';
   var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+  var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
+  var RemoteDataStore = App.RemoteDataStore;
   var FormHandler = App.FormHandler;
+  var Validation = App.Validation;
   var CheckList = App.CheckList;
-  var myTruck = new Truck('ncc-1701', new DataStore());
+  var remoteDS = new RemoteDataStore(SERVER_URL);
+  var webshim = window.webshim;
+  var myTruck = new Truck('ncc-1701', remoteDS);
 
   window.myTruck = myTruck;
   var checkList = new CheckList(CHECKLIST_SELECTOR);
@@ -19,4 +24,10 @@
     myTruck.createOrder.call(myTruck, data);
     checkList.addRow.call(checkList, data);
     });
+
+    formHandler.addInputHandler(Validation.isCompanyEmail);
+
+    webshim.polyfill('forms forms-ext');
+  webshim.setOptions('forms', { addValidators: true, lazyCustomMessages: true });
+
   })(window);
